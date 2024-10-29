d84ce8ec7e7eb53ae9dcfac28a9d9b6bb1fece22
Jongkuk Lim <lim.jeikei@gmail.com>
2024-10-29 17:33:56 +0900

    Add react webcam test.

---
+++ react/webcam-test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import WebcamCapture from "./components/WebcamCapture";
+
+const App: React.FC = () => {
+  return (
+    <WebcamCapture />
+  );
+};
+
+export default App;
